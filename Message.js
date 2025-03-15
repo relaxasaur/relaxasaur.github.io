b9e7677cd9846d0b42b1324e27bc2256588b30dc
Ian Weber <kentuckyfriedpunk@yahoo.com>
2025-03-15 17:14:04 -0400

    Add Message.js

---
+++ Message.js
@@ -0,0 +1,21 @@
+import React from 'preact';
+import { render } from 'preact';
+import { useState } from 'preact/hooks';
+
+function Message() {
+    const [cravePoints, setPoints] = useState(0);
+    function increment() {
+        setPoints(cravePoints+1)
+    }
+
+    return (
+        <div>
+            <h5>{cravePoints} crave points</h5>
+            <button id="pressable" onClick={increment}></button>     
+        </div>       
+    );
+}
+
+const target = document.getElementById('message');
+render(<Message />, target);
+
---
+++ dist/index.e6b5c606.css.29820.d
@@ -0,0 +1,31 @@
+body {
+  text-align: center;
+  background-color: #87cefa;
+}
+
+h1 {
+  color: #adff2f;
+  text-shadow: -.5rem .5rem green;
+  margin-bottom: 0;
+  font-family: Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif;
+  font-size: 80px;
+}
+
+h5 {
+  color: navy;
+  margin: 0;
+  font-family: Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif;
+  font-size: 30px;
+}
+
+#timer {
+  color: navy;
+  margin-top: 1rem;
+  font-family: Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif;
+  font-size: 50px;
+}
+
+.egg {
+  transform: translateY(25vh)scale3d(2.5, 2.5, 1);
+}
+/*# sourceMappingURL=index.e6b5c606.css.map */
